66c552d78e28b8aa4a94bb63ec9c24d0bd874a9b
Vaibhav <vaibhavpanchal205@gmail.com>
2024-12-22 18:41:40 +0530

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 const App = () => {
   return (
@@ -12,8 +15,13 @@ const App = () => {
       <div className="container">
         <Title subTitle='Our PROGRAM' title='What We Offer'/>
         <Programs/>
-      </div>
-    </div>
+        <About/>
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Campus/>
+        <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+        <Testimonials/>
+     </div>
+  </div>
   )
 }
 
